a4af9a5b92dadc96e6d6120efba4e6ee802d54c8
Marcie Collins <marciecollins12@gmail.com>
2026-05-18 02:28:10 +1000

    yes

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from PySide6.QtWidgets import (
 from PySide6.QtGui import QFont, QPixmap
 from PySide6.QtCore import Qt
 
-
-
 firstEntry = 0
 
 # Creating the Window
@@ -23,13 +21,15 @@ seriesImage = QPixmap("series.png")
 
 # Create the image labels and set the images
 characterImageLabel = QLabel()
-characterImageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-characterImageLabel.picture = characterImage
-characterImageLabel.setObjectName("characterImageLabel")
+characterImageLabel.setText("AleksLe Coming Soon...")
+# characterImageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+# characterImageLabel.setPixmap(characterImage)
+# characterImageLabel.setObjectName("characterImageLabel")
 seriesImageLabel = QLabel()
-seriesImageLabel.setPixmap(seriesImage)
-seriesImageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-seriesImageLabel.setObjectName("seriesImageLabel")
+seriesImageLabel.setText("Series Coming Soon...")
+# seriesImageLabel.setPixmap(seriesImage)
+# seriesImageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+# seriesImageLabel.setObjectName("seriesImageLabel")
 
 # align the widgets in the grid layout and add them to the window
 window.setLayout(gridLayout)
