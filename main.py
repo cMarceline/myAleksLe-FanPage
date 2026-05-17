98bc161dcd505eb59e6ce16651dde8d28dabb512
Marcie Collins <marciecollins12@gmail.com>
2026-05-18 08:02:04 +1000

    this might work?

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def regexSearch(searchText, fullString) -> str:
     # Original Regex for returning lines containing the search text
     # ^(.*)(\bSEARCHTEXT\b)(.*)
     findallString = r"^(.*)(\b" + searchText + r"\b)(.*)"
-    return sub(findallString, fullString, MULTILINE)
+    return sub(findallString, "", fullString)
 
 def regexFilter(filterText, fullString, categoryNumber) -> str:
     # Raw REGEX
